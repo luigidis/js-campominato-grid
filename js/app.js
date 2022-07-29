edaa728a2eab15b7796c25b43050149c08f3565d
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-29 17:31:56 +0200

    Ex End

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // console.log('yo');
-
+console.log(chooseDifficulty())
+// const rigaCelle = chooseDifficulty();
+// console.log(rigaCelle)
 // stabilisco numero di celle che deve avere una riga
-const rigaCelle = chooseDifficulty();
-console.log(rigaCelle)
+
 // calcolo totalità delle celle che avrò nella mia griglia
 // const celleTotali = 10 ** 2;
 // vado a prendere l'elemento nel dom dove dovrò mettere le mie celle
@@ -13,9 +14,20 @@ const playButton = document.querySelector('.play');
 // console.log(playButton);
 // a questo punto mi creo una funzione che mi crea un div di classe celle
 function createGrid (celle) {
+    container.classList.remove('grid_template_hard', 'grid_template_medium', 'grid_template_easy')
+    // container.classList.remove('grid_template_medium')
+    // container.classList.remove('grid_template_easy')
     container.innerHTML = ''
-    numeroCelle = rigaCelle ** 2
-
+    numeroCelle = celle ** 2
+    // Qui cambierò la classe in base alla mia griglia
+    if (numeroCelle === 100) {
+        container.classList.add('grid_template_hard')
+        
+    } else if (numeroCelle === 81) {
+        container.classList.add('grid_template_medium')
+    } else {
+        container.classList.add('grid_template_easy')
+    }
     for (let i = 0; i < numeroCelle; i++) {
         // Evoco la mia funzione per creare le celle
         const cella = createCell ();
@@ -25,6 +37,7 @@ function createGrid (celle) {
         container.append(cella);
     }
 }
+
 //invoco la mia funzione per creare celle
 // createGrid(rigaCelle)
 
@@ -49,8 +62,12 @@ function clickCheck(event) {
 
 // voglio che la mia griglia appaia solo quando l'utente clicca il tasto gioca
 // console.log(playButton);
-playButton.addEventListener('click', chooseDifficulty, createGrid);
-
+// playButton.addEventListener('click', chooseDifficulty)
+// playButton.addEventListener('click', createGrid)
+playButton.addEventListener('click', function(){
+    createGrid(chooseDifficulty());
+});
+    
 // const easy = document.querySelector('option[value="easy"]');
 // console.log(easy);
 
@@ -60,7 +77,7 @@ playButton.addEventListener('click', chooseDifficulty, createGrid);
 // const hard = document.querySelector('option[value="hard"]');
 // console.log(hard);
 
-function chooseDifficulty(num) {
+function chooseDifficulty() {
     // prendo l'elemento nel dom della selection
     const difficulty = document.getElementById('difficulty');
     // trasformo il dato della value trovata in Intero
@@ -68,17 +85,19 @@ function chooseDifficulty(num) {
     // console.log(difficultyInt, typeof difficultyInt);
     // In base alla difficoltà scelta cambio il numero di caselle
     if (difficultyInt === 1) {
-        console.log('ciao');
-        return num = 7;
+        console.log('difficoltà 1');
+        num = 7;
     } else if (difficultyInt === 2) {
         console.log('difficoltà 2');
-        return num = 9;
+        num = 9;
     } else {
         console.log ('difficoltà 3');
-        return num = 10;
+        num = 10;
     }
-    
+    return num
 }
+   
+    
 
     
 
