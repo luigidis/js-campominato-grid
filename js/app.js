828a3475497dd8a5b78bbeb841949822291db68c
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-29 16:44:20 +0200

    Funzione per il cambio di difficoltà creata

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // console.log('yo');
+
 // stabilisco numero di celle che deve avere una riga
-const rigaCelle = 10;
+const rigaCelle = chooseDifficulty();
+console.log(rigaCelle)
 // calcolo totalità delle celle che avrò nella mia griglia
 // const celleTotali = 10 ** 2;
 // vado a prendere l'elemento nel dom dove dovrò mettere le mie celle
@@ -11,6 +13,7 @@ const playButton = document.querySelector('.play');
 // console.log(playButton);
 // a questo punto mi creo una funzione che mi crea un div di classe celle
 function createGrid (celle) {
+    container.innerHTML = ''
     numeroCelle = rigaCelle ** 2
 
     for (let i = 0; i < numeroCelle; i++) {
@@ -41,26 +44,45 @@ function clickCheck(event) {
     // metto un colore alla mia cella se viene cliccata
     cell.classList.toggle('clicked');
     // metto un messaggio in console della cella clickata
-    console.log(this.innerHTML);
+    // console.log(this.innerHTML);
 }
 
 // voglio che la mia griglia appaia solo quando l'utente clicca il tasto gioca
-console.log(playButton);
-playButton.addEventListener('click', createGrid);
+// console.log(playButton);
+playButton.addEventListener('click', chooseDifficulty, createGrid);
 
-const easy = document.querySelector('option[value="easy"]');
-console.log(easy);
+// const easy = document.querySelector('option[value="easy"]');
+// console.log(easy);
 
-const medium = document.querySelector('option[value="medium"]');
-console.log(medium);
+// const medium = document.querySelector('option[value="medium"]');
+// console.log(medium);
 
-const hard = document.querySelector('option[value="hard"]');
-console.log(hard);
+// const hard = document.querySelector('option[value="hard"]');
+// console.log(hard);
 
-const difficulty = document.getElementById('difficulty');
-console.log(difficulty);
-console.dir(difficulty);
-console.log(difficulty.value);
+function chooseDifficulty(num) {
+    // prendo l'elemento nel dom della selection
+    const difficulty = document.getElementById('difficulty');
+    // trasformo il dato della value trovata in Intero
+    difficultyInt = parseInt(difficulty.value);
+    // console.log(difficultyInt, typeof difficultyInt);
+    // In base alla difficoltà scelta cambio il numero di caselle
+    if (difficultyInt === 1) {
+        console.log('ciao');
+        return num = 7;
+    } else if (difficultyInt === 2) {
+        console.log('difficoltà 2');
+        return num = 9;
+    } else {
+        console.log ('difficoltà 3');
+        return num = 10;
+    }
+    
+}
+
+    
+
+
     
    
 
